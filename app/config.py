5e52e1e710f414d2ac39bb07e613bd94547832be
David Navarro <josewjjunco@gmail.com>
2025-06-11 23:44:26 -0500

    nit: Estilos y vistas del admin modificadas

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ class DevelopmentConfig:
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_HOST = os.getenv('DB_HOST')
-    DB_PORT = os.getenv('DB_PORT', 3306)
+    DB_PORT = os.getenv('DB_PORT', 3405)
     DB_NAME = os.getenv('DB_NAME')
 
     # ----- URI DE CONEXIÓN -----
