6a4f92239ca41a3dc17d10e221d11b9c367eafb8
David Navarro <josewjjunco@gmail.com>
2025-07-14 12:32:32 -0500

    feat: modelo SQLAlchemy de las tablas carritos e items_carrito añadidos

---
+++ app/models/carrito.py
@@ -0,0 +1,19 @@
+from app import db
+
+class Carrito(db.Model):
+    __tablename__ = 'carritos'
+
+    id_carrito = db.Column(db.Integer, primary_key=True)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuario'))
+    
+    usuario = db.relationship('Usuario', backref=db.backref('carritos', lazy=True))
+    items = db.relationship('ItemCarrito', backref='carrito', cascade='all, delete-orphan', lazy=True)
+
+class ItemCarrito(db.Model):
+    __tablename__ = 'items_carrito'
+
+    id_carrito = db.Column(db.Integer, db.ForeignKey('carritos.id_carrito'), primary_key=True)
+    id_producto = db.Column(db.Integer, db.ForeignKey('productos.id_producto'), primary_key=True)
+    cantidad = db.Column(db.Integer, nullable=False)
+
+    producto = db.relationship('Producto', backref='items_carrito')
