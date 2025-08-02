73e16a7790bb4440948831c135fdd7ccaf58fe37
David Navarro <josewjjunco@gmail.com>
2025-08-02 10:08:25 -0500

    fix: Date time añadido para el guardado de hora exacta

--- app/models/pedidos.py
+++ app/models/pedidos.py
@@ -11,8 +11,8 @@ class Pedido(db.Model):
 
     id_pedido = db.Column(db.Integer, primary_key=True, autoincrement=True)
     id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuario'), nullable=False)
-    fecha_pedido = db.Column(db.Date, nullable=False)
-    fecha_entrega = db.Column(db.Date, nullable=False)
+    fecha_pedido = db.Column(db.DateTime, nullable=False)
+    fecha_entrega = db.Column(db.DateTime, nullable=False)
     valor_total = db.Column(db.Numeric(10, 2), nullable=False)
     id_estado = db.Column(db.Integer, db.ForeignKey('estados.id_estado'), nullable=False)
 
