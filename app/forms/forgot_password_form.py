02184ebe5158eb5b91c5ee73683a2d7aaf9f2e7f
Jeison Gutierrez <jeisonalejandrogutierrezbaraja@gmail.com>
2025-08-27 18:56:06 -0500

    Corrección de errores

--- app/forms/forgot_password_form.py
+++
@@ -1,8 +0,0 @@
-#Formulario para pedir token de recuperación de contraseña
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
-
-class ForgotPasswordForm(FlaskForm):
-    email = StringField('Correo Electrónico', validators=[DataRequired(), Email()])
-    submit = SubmitField('Enviar instrucciones')
--- app/static/js/header.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-// desplegar - ocultar, el menu hamburguesa 
-toggle.addEventListener('click', () => {
-    menu.classList.toggle('show');
-});
