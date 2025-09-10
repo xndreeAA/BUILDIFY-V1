033163d5ce8609de3bfe6f25539a27730b120e2e
Jeison Gutierrez <jeisonalejandrogutierrezbaraja@gmail.com>
2025-09-10 01:17:29 -0500

    Feat: Se agrega logica de acceso segun rol

--- app/modules/usuarios/routes/admin_routes.py
+++ app/modules/usuarios/routes/admin_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, render_template, request, abort
 from flask_login import login_required, current_user
+from app.modules.auth.utils.decorations import admin_required
 from app.core.models.rol import Rol 
 import os
     
@@ -19,12 +20,14 @@ def dashboard():
     return render_template('home.html', nombre=current_user.nombre)
 
 @admin_routes_bp.route('/crud-usuarios')
+@admin_required
 @login_required
 def crud_usuarios():
     return render_template('crud_usuarios.html')
 
 
 @admin_routes_bp.route('/crear_usuarios')
+@admin_required
 @login_required
 def crear_usuarios():
     roles = Rol.query.all()   
